6ffb8cfdc869dddf15b6cb70564aecc8d083fde3
Pasindu-Weerasinghe <weerasinghewpps@gmail.com>
2023-07-14 23:59:10 +0530

    side bar

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
-import Image from 'next/image'
+import SideNav from './components/SideNav/page'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     weda d sagor
-     test
+    <main>
+     <SideNav/>
     </main>
   )
 }
