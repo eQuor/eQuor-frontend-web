63e89038dd0f17977e3fcc07209833a85fade9c2
Pasindu-Weerasinghe <weerasinghewpps@gmail.com>
2023-07-17 17:49:32 +0530

    add nav bar

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 
+import Navbar from './components/Navbar/page'
 import SideNav from './components/SideNav/page'
 
 export default function Home() {
   return (
     <main>
+      <Navbar/>
      <SideNav/>
     </main>
   )
