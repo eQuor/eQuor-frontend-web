7552a6d5ad400ccf11a667eab497a65bf7bea4e4
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-07-18 12:18:00 +0530

    Update Navbar.js

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -10,21 +10,21 @@
 
 // export default Navbar
 
-'use client'
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
 //import React, { useState } from 'react'
-import Button from './Button'
-import 'tailwindcss/tailwind.css'
-import logo from './eQuor.svg'
-import { links } from './Data'
-import { Butterfly_Kids } from 'next/font/google'
-import { IonIcon } from '@ionic/react'
-import '@ionic/react/css/core.css'
-import { menu, close } from 'ionicons/icons'
-import { useState } from 'react'
+import Button from "./Button";
+import "tailwindcss/tailwind.css";
+import logo from "./eQuor.svg";
+import { links } from "./Data";
+import { Butterfly_Kids } from "next/font/google";
+import { IonIcon } from "@ionic/react";
+import "@ionic/react/css/core.css";
+import { menu, close } from "ionicons/icons";
+import { useState } from "react";
 
 const Navbar = () => {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
   return (
     <div className="shadow-md w-full fixed top-0 left-0">
@@ -41,28 +41,29 @@ const Navbar = () => {
         {/* <ul className="md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-[#0066FF] md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in"> */}
         <ul
           className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-[#0066FF] md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-            open ? 'top-20' : 'top-[-300px]'
+            open ? "top-20" : "top-[-300px]"
           }`}
         >
           {links.map((link) => {
-            const { id, url, text } = link
+            const { id, url, text } = link;
             return (
               <li key={id} className="md:ml-8 text-xl md:my-0 my-3">
                 <a
                   href="#"
                   className="text-white font-small hover:text-black duration-500"
-                  
                 >
                   {text}
                 </a>
               </li>
-            )
+            );
           })}
           <Button>Login</Button>
         </ul>
       </div>
+          
+      
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
