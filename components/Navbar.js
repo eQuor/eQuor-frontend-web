70fb958fc3dd77665d924800f323d332fbd72afc
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-08-01 13:47:18 +0530

    update navbar

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+      <nav class="fixed top-0 left-0 z-50 bg-white w-full py-4 ">
+        <h1>Hiiiii</h1>
+      </nav>
+    )
+}
+
+export default Navbar
