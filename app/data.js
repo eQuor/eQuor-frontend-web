637efe1cf194b0a43975e5069b399051e2cdd199
Pasindu-Weerasinghe <91451835+Pasindu-Weerasinghe@users.noreply.github.com>
2023-08-01 23:22:15 +0530

    create side bar

---
+++ app/data.js
@@ -0,0 +1,10 @@
+const menu = [
+  { topic: false, subitem: [{ title: "Dashboard", icon: true }] },
+  {
+    topic: "user management",
+    subitem: [
+      { title: "Lecturer", icon: true },
+      { title: "Student", icon: true },
+    ],
+  },
+];
