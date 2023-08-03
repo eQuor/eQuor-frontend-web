4e9f333ce6e113dc3e1b6a5dccdce59cf26bd8f0
Pasindu-Weerasinghe <weerasinghewpps@gmail.com>
2023-08-04 01:18:06 +0530

    lecturer home page create

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 import React from "react";
-// import Card from "@components/card";
+import Card from "@components/card";
+import Image from "next/image";
 
 const Home = () => {
   return (
     <> 
-   
+    <div>
+      {/* <Image src={}></Image> */}
+    </div>
     </>
   );
 };
