d5a32845d0088d803db6a4883da055a37674b357
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-08-08 00:42:06 +0530

    create folder structure

---
+++ app/admin/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="col-start-1 col-end-13 row-start-1 row-end-1  px-4 pt-5 mt-3">
+      <span className="text-light-blue font-semibold text-lg">Dashboard</span>
+
+      <p className="text-link-ash font-semibold text-sm">
+        Home /{' '}
+        <span className="text-black font-semibold text-sm">Dashboard</span>
+      </p>
+    </div>
+  )
+}
+
+export default page
