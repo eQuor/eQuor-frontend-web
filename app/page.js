71a7e19a4a7b3cf024588f564fd4c5ad27f6a619
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-08-08 22:35:50 +0530

    create staff member registration form

--- app/page.js
+++ app/page.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Link from "next/link";
 
 const Home = () => {
   return (
     // <div className="bg-green-600 col-start-3 col-end-4 row-start-3 row-end-4">
     //   <button className="bg-blue-700 min-w-[50px]">test</button>
     // </div>
-    <></>
+
+    <div>
+      Welcome to the home page
+  
+    </div>
+    
   );
 };
 
