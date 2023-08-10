ac7c6f1740629bc9ad2b4fd4a73c9061970fe0b8
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-08-11 00:53:49 +0530

    Create page.js

---
+++ app/admin/page.js
@@ -0,0 +1,38 @@
+'use client'
+import React from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+//import { useRouter } from 'next/router'
+
+const page = () => {
+  const router = useRouter()
+
+  const handleClick = () => {
+    router.push('/admin/staff')
+  }
+  return (
+    <>
+      <div className="col-start-1 col-end-13 row-start-1 row-end-1  px-4 pt-5 mt-3">
+        <span className="text-light-blue font-semibold text-lg">Dashboard</span>
+
+        <p className="text-link-ash font-semibold text-sm">
+          Home /{' '}
+          <span className="text-black font-semibold text-sm">Dashboard</span>
+        </p>
+
+        {/* <Link href="/admin/staffmember">Admin</Link> */}
+      </div>
+
+      <div className="col-start-6 col-end-9 row-start-3 row-end-3 ">
+        <button
+          className=" text-white  bg-light-blue hover:bg-blue-800  font-medium rounded-lg text-sm px-20 py-2  "
+          onClick={handleClick}
+        >
+          Add Staff Member
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default page
