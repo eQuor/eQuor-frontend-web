90b74cea22f78465cdaba2feb86fc11badfd1f42
madushatharindi <madushadesilvad6@gmail.com>
2023-08-10 23:26:59 +0530

    change

--- app/students/page.js
+++ app/students/page.js
@@ -18,18 +18,17 @@ function page() {
   // }, []);
 
   const data = [
-    { id: 1,name: 'Manag Lama', reg: '2020/IS/027',ind: 20020279, email: 'maan@gmail.com', add: 'molamo',age: 23 },
-    { id: 1,name: 'Manag Lama', reg: '2020/IS/027',ind: 20020279, email: 'maan@gmail.com', add: 'molamo',age: 23 },
-    { id: 1,name: 'Manag Lama', reg: '2020/IS/027',ind: 20020279, email: 'maan@gmail.com', add: 'molamo',age: 23 },
-    { id: 1,name: 'Manag Lama', reg: '2020/IS/027',ind: 20020279, email: 'maan@gmail.com', add: 'molamo',age: 23 },
-    { id: 1,name: 'Manag Lama', reg: '2020/IS/027',ind: 20020279, email: 'maan@gmail.com', add: 'molamo',age: 23 },
-    { id: 1,name: 'Manag Lama', reg: '2020/IS/027',ind: 20020279, email: 'maan@gmail.com', add: 'molamo',age: 23 },
-    { id: 1,name: 'Manag Lama', reg: '2020/IS/027',ind: 20020279, email: 'maan@gmail.com', add: 'molamo',age: 23 },
-    { id: 1,name: 'Manag Lama', reg: '2020/IS/027',ind: 20020279, email: 'maan@gmail.com', add: 'molamo',age: 23 },
-    { id: 1,name: 'Manag Lama', reg: '2020/IS/027',ind: 20020279, email: 'maan@gmail.com', add: 'molamo',age: 23 },
-    { id: 1,name: 'Manag Lama', reg: '2020/IS/027',ind: 20020279, email: 'maan@gmail.com', add: 'molamo',age: 23 },
-    { id: 1,name: 'Manag Lama', reg: '2020/IS/027',ind: 20020279, email: 'maan@gmail.com', add: 'molamo',age: 23 },
-    { id: 1,name: 'Manag Lama', reg: '2020/IS/027',ind: 20020279, email: 'maan@gmail.com', add: 'molamo',age: 23 },
+    { id: 1,name: 'Manag Lama', reg: '2020/IS/027',ind: 20020279, email: 'maan@gmail.com', add: 'molamo' },
+    { id: 1,name: 'Manag Lama', reg: '2020/IS/027',ind: 20020279, email: 'maan@gmail.com', add: 'molamo' },
+    { id: 1,name: 'Manag Lama', reg: '2020/IS/027',ind: 20020279, email: 'maan@gmail.com', add: 'molamo' },
+    { id: 1,name: 'Manag Lama', reg: '2020/IS/027',ind: 20020279, email: 'maan@gmail.com', add: 'molamo' },
+    { id: 1,name: 'Manag Lama', reg: '2020/IS/027',ind: 20020279, email: 'maan@gmail.com', add: 'molamo' },
+    { id: 1,name: 'Manag Lama', reg: '2020/IS/027',ind: 20020279, email: 'maan@gmail.com', add: 'molamo' },
+    { id: 1,name: 'Manag Lama', reg: '2020/IS/027',ind: 20020279, email: 'maan@gmail.com', add: 'molamo' },
+    { id: 1,name: 'Manag Lama', reg: '2020/IS/027',ind: 20020279, email: 'maan@gmail.com', add: 'molamo' },
+    { id: 1,name: 'Manag Lama', reg: '2020/IS/027',ind: 20020279, email: 'maan@gmail.com', add: 'molamo' },
+    { id: 1,name: 'Manag Lama', reg: '2020/IS/027',ind: 20020279, email: 'maan@gmail.com', add: 'molamo' },
+    { id: 1,name: 'Manag Lama', reg: '2020/IS/027',ind: 20020279, email: 'maan@gmail.com', add: 'molamo' },
   
   ];
   
@@ -40,7 +39,7 @@ function page() {
     { Header: 'Index No', accessor: 'ind' },
     { Header: 'Email', accessor: 'email' },
     { Header: 'Address', accessor: 'add' },
-    { Header: 'Age', accessor: 'age' },
+    // { Header: 'Age', accessor: 'age' },
   ];
   
   
@@ -51,23 +50,35 @@ function page() {
   return (
    
       
-      <div>
-         <br></br>
-        <span className="font-Open Sans font-semibold text-2xl text-blue-600 pl-8">Student Management</span>
-        <br></br>
-        <span className="font-Open Sans text-xs text-gray-500 pl-8">Home / Student </span>
-        <br></br>
-        <button className="col-start-10 col-end-12 row-start-1 row-end-1 absolute right-96 bg-blue-600 text-white text-sm p-5 rounded-lg pl-8" style={{ fontSize: '20px' }}>Add New Student</button>
-        <br></br>
-        <h1 className="font-Open Sans text-4xl text-blue-900 pl-8">Current Students</h1>
-        <br></br>
-        <br></br>
-        <div className='col-start-1 col-end-13 row-start-1 row-end-7'>
-        <DataTable columns={columns} data={data} />
-        </div>
-      </div>
+      // <div>
+        
+      //   <span className="font-Open Sans font-semibold text-2xl text-blue-600 pl-8">Student Management</span>
+       
+      //   <span className="font-Open Sans text-xs text-gray-500 pl-8">Home / Student </span>
+        
+      //   <button className="col-start-10 col-end-12 row-start-1 row-end-1 absolute right-96 bg-blue-600 text-white text-sm p-5 rounded-lg pl-8" style={{ fontSize: '20px' }}>Add New Student</button>
       
-  
+      //   <h1 className="font-Open Sans text-4xl text-blue-900 pl-8">Current Students</h1>
+        
+      //   <div className=' col-start-1  '>
+      //   <DataTable columns={columns} data={data} />
+      //   </div>
+      // </div>
+      <>
+      <div className="col-start-1 col-end-13 row-start-1 row-end-1  px-4 pt-5 mt-3">
+        <span className="text-light-blue font-semibold text-lg">Student Management</span>
+
+        <p className="text-link-ash font-semibold text-sm">
+          Home / Modules /{' '}
+          <span className="text-black font-semibold text-sm">Student Management</span>
+        </p>
+      </div>
+      <div className="col-start-1 col-end-8 row-start-2 px-4 pt-5">
+      <DataTable columns={columns} data={data} />
+        </div>
+      
+
+      </>
     
   )
 }
