2a86a2f599bbb3a11db1f7df660520cf442559e6
Pasindu-Weerasinghe <weerasinghewpps@gmail.com>
2023-08-11 21:00:43 +0530

    create students home

---
+++ app/student/page.js
@@ -0,0 +1,41 @@
+"use client";
+import Card from "@components/Card";
+import Search from "@components/Search";
+import TabsContainer from "@components/Tabscontent";
+import React from "react";
+
+const page = () => {
+  //const pageTopic = "Modules";
+  return (
+    <>
+      <div className="col-start-1 col-end-13 ">
+        <TabsContainer/>
+      </div>
+      <div className="col-start-1 col-end-13 px-4 row-start-2 row-end-3">
+        <span className="text-light-blue font-semibold text-lg">
+          {/* {pageTopic} */}
+        </span>
+
+        <p className="text-link-ash font-semibold text-sm">
+          Home /{" "}
+          <span className="text-black font-semibold text-sm"></span>
+        </p>
+      </div>
+
+      <div className=" col-start-1 col-end-8 ml-8">
+        <Search />
+      </div>
+
+      <div className=" col-start-1 col-end-13 ml-10 flex ">
+        <div className=" ml-1 flex gap-6 flex-wrap">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default page;
