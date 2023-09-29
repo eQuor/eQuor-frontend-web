41e6ff2b26f8ab653ef0df4bac3c302da2d3c4fa
Senith Uthsara <senithkarunarathneu@gmail.com>
2023-09-29 19:37:21 +0530

    Chnage auth

--- app/student/device/page.js
+++ app/student/device/page.js
@@ -1,20 +1,13 @@
-'use client'
-import React from 'react'
-import ProtectedRouteWRap from '@app/ProtectedRouteWRap'
-import QRCode from 'react-qr-code'
-import { useState, useEffect} from 'react'
+"use client";
+import React from "react";
+import ProtectedRouteWRap from "@app/ProtectedRouteWRap";
+import QRCode from "react-qr-code";
+import { useState, useEffect } from "react";
 
-import { axiosGet } from '@common/basicAxios'
-import { useAuth } from '@contexts/authContext'
+import { axiosGet } from "@common/basicAxios";
+import { useAuth } from "@contexts/authContext";
 
-import axios from 'axios'
-
-
-
-
-
-
-
+import axios from "axios";
 
 const page = () => {
   // const [text, setText] = useState('')
@@ -22,58 +15,55 @@ const page = () => {
   //   setText(e.target.value)
   // }
 
-  const [data, setValue] = useState('')
+  const [data, setValue] = useState("");
 
-  const { authUser, setAuthUser } = useAuth()
+  const { authUser, setAuthUser } = useAuth();
 
- const getResponse = async () => {
-   const response = await axiosGet('/auth/getId')
-   if (response.status == 200) {
-     console.log('axios wed')
-     console.log(response.data)
-     
-     let value = response.data.toString()
-     setValue(value)
- 
-   } else {
-     console.log('error while fetching API')
-   }
-   return response
- }
- useEffect(() => {
-   console.log('useEffect is running')
-   const response = getResponse()
- }, [])
+  const getResponse = async () => {
+    const response = await axiosGet("/auth/getId");
+    if (response.status == 200) {
+      console.log("axios wed");
+      console.log(response.data);
 
-
-
-
+      let value = response.data.toString();
+      setValue(value);
+    } else {
+      console.log("error while fetching API");
+    }
+    return response;
+  };
+  useEffect(() => {
+    console.log("useEffect is running");
+    const response = getResponse();
+  }, []);
 
   return (
     <ProtectedRouteWRap>
-      <div className="col-start-1 col-end-13 row-start-1 row-end-1  px-4 pt-5 mt-3">
-        <span className="text-light-blue font-semibold text-lg">
+      <div className='col-start-1 col-end-13 row-start-1 row-end-1  px-4 pt-5 mt-3'>
+        <span className='text-light-blue font-semibold text-lg'>
           Your Device
         </span>
 
-        <p className="text-link-ash font-semibold text-sm">
-          Home /{' '}
-          <span className="text-black font-semibold text-sm">Your Device</span>
+        <p className='text-link-ash font-semibold text-sm'>
+          Home /{" "}
+          <span className='text-black font-semibold text-sm'>Your Device</span>
         </p>
       </div>
 
-      <div className="col-start-1 col-end-13 px-4 pt-5 ">
-        <div className="flex">
-          <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
-            <p className="mb-3 font-normal text-gray-500 ">
+      <div className='col-start-1 col-end-13 px-4 pt-5 '>
+        <div className='flex'>
+          <div className='max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow'>
+            <p className='mb-3 font-normal text-gray-500 '>
               1. Download eQuor mobile application.
             </p>
-            <p className="mb-3 font-normal text-gray-500 ">
+            <p className='mb-3 font-normal text-gray-500 '>
               2. Open the Web Application
             </p>
-            <p className="mb-3 font-normal text-gray-500 ">3. Scan the QR Code</p>
+            <p className='mb-3 font-normal text-gray-500 '>
+              3. Scan the QR Code
+            </p>
           </div>
-          <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow ml-5">
+          <div className='max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow ml-5'>
             <div>
               <h1>QR Code</h1>
               <QRCode value={data} />
@@ -94,7 +84,7 @@ const page = () => {
         </div>
       </div>
     </ProtectedRouteWRap>
-  )
-}
+  );
+};
 
-export default page
+export default page;
