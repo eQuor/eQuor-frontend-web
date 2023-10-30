886d440328e096ee16cf5776a3d5c85b9eead663
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-10-30 10:39:03 +0530

    csv student and lecturer

--- app/staff/module/[slug]/page.js
+++ app/staff/module/[slug]/page.js
@@ -2,14 +2,17 @@
 import React from 'react'
 import ProtectedRouteWRap from '@app/ProtectedRouteWRap'
 import { useEffect, useState, useRef } from 'react'
-import { axiosGet } from '@common/basicAxios'
+import { axiosGet, axiosPost } from '@common/basicAxios'
 import { useAuth } from '@contexts/authContext'
 import axios from 'axios'
-import Group from '@components/Group'
+
 import Papa from 'papaparse'
+import Swal from 'sweetalert2'
+import { useRouter } from 'next/navigation'
+import CsvLecturer from '@components/CsvLecturer'
+import CsvStudent from '@components/CsvStudent'
 
 const page = ({ params }) => {
-
   //Show moudle name from the backend
   const [data, setResponse] = useState('')
 
@@ -33,97 +36,26 @@ const page = ({ params }) => {
     getResponse()
   }, [])
 
-
-
-  //Show data in the CSV
-  // const [csvData, setCsvData] = useState([])
-  // const handleFileUpload = (e) => {
-  //   const file = e.target.files[0]
-  //   Papa.parse(file, {
-  //     header: true,
-  //     dynamicTyping: true,
-  //     complete: function (results) {
-  //       console.log(results.data)
-  //       setCsvData(results.data)
-  //     },
-  //   })  
-  //  }
-  
-  const fileInput = useRef()
-
-  const parseCsv = () => {
-    const file = fileInput.current.files[0]
-    Papa.parse(file, {
-      complete: function (results) {
-        console.log(results)
-      },
-    })
-  }
-  
  
+return (
+  <ProtectedRouteWRap>
+    <div className="col-start-1 col-end-13 row-start-1 row-end-1  px-4 pt-5 mt-3">
+      <span className="text-light-blue font-semibold text-lg">{data.name}</span>
 
+      <p className="text-link-ash font-semibold text-sm">
+        Home / Modules /{' '}
+        <span className="text-black font-semibold text-sm">{data.name}</span>
+      </p>
+    </div>
 
-  return (
-    <ProtectedRouteWRap>
-      {/* <div>{params.slug}</div> */}
-      <div className="col-start-1 col-end-13 row-start-1 row-end-1  px-4 pt-5 mt-3">
-        <span className="text-light-blue font-semibold text-lg">
-          {data.name}
-        </span>
-
-        <p className="text-link-ash font-semibold text-sm">
-          Home / Modules /{' '}
-          <span className="text-black font-semibold text-sm">{data.name}</span>
-        </p>
+    <div className="col-start-1 col-end-13 row-start-2 px-4 pt-5 mt-3">
+      <div className="grid grid-cols-1 grid-rows-1 md:grid-cols-2">
+        <CsvLecturer prop={data.name} />
+        <CsvStudent prop={data.name} />
       </div>
-
-      <div className="col-start-1 col-end-13 row-start-2 px-4 pt-5 mt-3">
-        <div className="grid grid-cols-1 grid-rows-1 md:grid-cols-2">
-          <div className="mr-4 block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]">
-            <div className="p-6">
-              <h5 className="text-lg font-bold leading-none text-dark-blue mt-3">
-                Assign Lectures
-              </h5>
-              {/* <button
-                type="button"
-                class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-3"
-              >
-                Create Group
-              </button> */}
-              <input type="file" ref={fileInput} accept=".csv" />
-              <button
-                onClick={parseCsv}
-                type="button"
-                class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-3"
-              >
-                Regiter
-              </button>
-          
-            </div>
-          </div>
-
-          <div className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]">
-            <div className="p-6">
-              <h5 className="text-lg font-bold leading-none text-dark-blue mt-3 ">
-                Assign Students
-              </h5>
-              <button
-                type="button"
-                class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-3"
-              >
-                Create Group
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="grid grid-cols-1 grid-rows-1 md:grid-cols-2 mt-2">
-          {/* <Group />
-
-          <Group /> */}
-        </div>
-      </div>
-    </ProtectedRouteWRap>
-  )
+    </div>
+  </ProtectedRouteWRap>
+)
 }
 
 export default page
---
+++ components/CsvLecturer.js
@@ -0,0 +1,104 @@
+'use client'
+import React from 'react'
+import ProtectedRouteWRap from '@app/ProtectedRouteWRap'
+import { useEffect, useState, useRef } from 'react'
+import { axiosGet, axiosPost } from '@common/basicAxios'
+import { useAuth } from '@contexts/authContext'
+import axios from 'axios'
+import Papa from 'papaparse'
+import Swal from 'sweetalert2'
+import { useRouter } from 'next/navigation'
+
+const Csv = ({ props }) => {
+  const module_name = props
+  const fileInput = useRef()
+  const [csvData, setCsvData] = useState([])
+  const router = useRouter()
+  const { authUser, setAuthUser } = useAuth()
+
+    
+    
+  const parseCsv = () => {
+    const file = fileInput.current.files[0]
+    Papa.parse(file, {
+      header: true,
+      dynamicTyping: true,
+      complete: function (results) {
+        console.log(results.data)
+        const data = []
+        results.data.forEach((element) => {
+          if (element.lecturer_id != null) {
+            data.push(element)
+          }
+        })
+
+        setCsvData(data)
+        console.log('After passing data')
+        handleSubmit()
+      },
+    })
+  }
+
+  useEffect(() => {
+    console.log(csvData)
+  }, [csvData])
+
+  const handleSubmit = () => {
+    // e.preventDefault()
+
+    Swal.fire({
+      title: 'Are you sure?',
+     text: "Add lecturers to the" + " "+ module_name +"!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#4154F1',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, Save it!',
+    })
+      .then(async (result) => {
+        if (result.isConfirmed) {
+        
+          const response = await axiosPost(
+            '/staff/lecturer-modules',
+            csvData,
+            router,
+            setAuthUser
+          )
+          
+          if (response.status === 200) {
+            Swal.fire('Saved!', 'New records have been saved.', 'success')
+            // router.push('/staff/module')
+          }
+          //  else {
+          //   Swal.fire('Error!', "Couldn't save new staff members.", 'success')
+          // }
+        }
+      })
+      .catch((error) => {
+        Swal.fire('Error!', "Couldn't save new module", 'error')
+        alert(response.status)
+      })
+  }
+
+  return (
+    <>
+      <div className="mr-4 block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]">
+        <div className="p-6">
+          <h5 className="text-lg font-bold leading-none text-dark-blue mt-3">
+            Assign Lectures
+          </h5>
+          <input type="file" ref={fileInput} accept=".csv" />
+          <button
+            onClick={parseCsv}
+            type="button"
+            class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-3"
+          >
+            Assign
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Csv
