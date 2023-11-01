88a75cc472c0e9c94ea9208b7bcb238a83fb83b0
madushatharindi <madushadesilvad6@gmail.com>
2023-11-01 13:39:29 +0530

    img changed

--- app/aboutUs/page.js
+++ app/aboutUs/page.js
@@ -10,14 +10,11 @@ const page = () => {
 
     <>
       <div className="bg-gray-100 min-h-screen flex items-center ">
-      <div class="relative w-[600] h-[740px]">
+     <div class="relative w-[600] h-[740px]">
   <div class="bg-light-blue w-[600] h-[740px] absolute bottom-0 z-10"></div>
     <div class=" w-[650px] h-[720px] absolute top-[150px] left-5 z-20"><Image src={aboutus} alt='aboutus' className='w-[644px] h-[419px] z-10' /></div>
 </div>
-<div class="relative w-[600] h-[740px]">
-  <div class="bg-light-blue w-[600] h-[740px] absolute bottom-0 z-10"></div>
-    <div class=" w-[650px] h-[720px] absolute top-[150px] left-5 z-20"><Image src={aboutus} alt='aboutus' className='w-[644px] h-[419px] z-10' /></div>
-</div>          
+         
 
       </div>
     </>
